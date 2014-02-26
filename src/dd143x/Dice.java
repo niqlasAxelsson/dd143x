31f6dc39b6ef3d33ca30d1042c6473d504174fe8
ricl <ricl@VIT02.ug.kth.se>
2014-02-26 14:32:42 +0100

    Dice

--- src/dd143x/Dice.java
+++ src/dd143x/Dice.java
@@ -1,10 +1,22 @@
 package dd143x;
 
+import java.util.Random;
+
 public class Dice {
 	
+	public int value;
+	private Random random;
 	
 	public Dice() {
-		
+		random = new Random();
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void throwDice() {
+		value = random.nextInt(6) + 1;
 	}
 
 }
