4933c4698d14775454775440e166336b594c3a35
Niklas <niqlas.axelsson@gmail.com>
2014-03-26 13:32:25 +0100

    on

--- src/dd143x/ScoreCard.java
+++ src/dd143x/ScoreCard.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 
 public class ScoreCard {
 
-public static final int ones = 0;
+	public static final int ones = 0;
 	public static final int twos = 1;
 	public static final int threes = 2;
 	public static final int fours = 3;
@@ -19,44 +19,69 @@ public static final int ones = 0;
 	public static final int fullHouse = 12;
 	public static final int chance = 13;
 	public static final int yatzy = 14;
-	
+
 	public static final int bonus = 50;
-	
-	public int[] scoreValues = new int[15]; 
-	
+	public static final int pointsToBonus = 63;
+
+	public int[] scoreValues = new int[15];
+
 	public ScoreCard() {
-		for (int i = 0; i < scoreValues.length; i++){
+		for (int i = 0; i < scoreValues.length; i++) {
 			scoreValues[i] = -1;
 		}
-		
+
 	}
-	
+
 	public int finalScore() {
 		int sum = 0;
-		int pointsToBonus = 63;
-		
-		for(int i = 0; i < scoreValues.length; i++) {
+
+		for (int i = 0; i < scoreValues.length; i++) {
 			sum += scoreValues[i];
 			if (i == 5 && sum >= pointsToBonus) {
 				sum += bonus;
 			}
 		}
 		return sum;
-	}    
-	
-	public boolean isCardFilled(){
+	}
+
+	public boolean isCardFilled() {
 		return getEmptyIndexes().isEmpty();
 	}
-	
-	
-	public LinkedList<Integer> getEmptyIndexes(){
+
+	public LinkedList<Integer> getEmptyIndexes() {
 		LinkedList<Integer> emptyIndexes = new LinkedList<Integer>();
-		for(Integer i = 0; i < scoreValues.length ; i++){
-			if (scoreValues[i] == -1 ){
+		for (Integer i = 0; i < scoreValues.length; i++) {
+			if (scoreValues[i] == -1) {
 				emptyIndexes.add(i);
 			}
 		}
 		return emptyIndexes;
+	}
+
+
+	/**
+	 * returns -1 if under par, 0 if on par and 1 if over par
+	 * @return
+	 */
+	public int onPar(){
+		int parScore = 0;
+		
+		for (int i = 0; i < sixes; i ++){
+			if (scoreValues[i] >= 0){
+				parScore += scoreValues[i];
+			}else{
+				parScore += i*3;
+			}
+		}
+		
+		if( parScore > pointsToBonus){
+			return 1;
+		}
+		if (parScore == pointsToBonus){
+			return 0;
+		}
+		
+		return -1;
 	}
 
 }
